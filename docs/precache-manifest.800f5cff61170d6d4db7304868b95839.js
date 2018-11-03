8e02341de2aca4b5c36a139bb9f9c790352645dd
Viktor Movchaniuk <vityamovchanyk@gmail.com>
2018-11-03 14:50:44 +0200

    add docs folder for gitPages

---
+++ docs/precache-manifest.800f5cff61170d6d4db7304868b95839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef8259d57657b60bc27d",
+    "url": "/static/js/main.ef8259d5.chunk.js"
+  },
+  {
+    "revision": "e5f69f5252a8285a0342",
+    "url": "/static/js/1.e5f69f52.chunk.js"
+  },
+  {
+    "revision": "ef8259d57657b60bc27d",
+    "url": "/static/css/main.61cdb963.chunk.css"
+  },
+  {
+    "revision": "e5f69f5252a8285a0342",
+    "url": "/static/css/1.57c057ae.chunk.css"
+  },
+  {
+    "revision": "b45376868b717dcdb6b485fb6adf122c",
+    "url": "/index.html"
+  }
+];
